d199275a6388b6a7579be5651f8e0e041354839c
Ricardo Pereira <ricardo.agp.914@gmail.com>
2017-02-28 15:08:44 +0000

    comentarios no codigo

--- Cone.cpp
+++ Cone.cpp
@@ -8,24 +8,27 @@ vector<Vertex*> createSphere(float radius, float height, int slice, int stack){
 	float h = height;
 	float l = sqrt(pow(h,2) + pow(radius,2))/stack;
 	float t = radius/stack;
+	float dimSide = 2*M_PI/slice;
 
 	for (int i = 0; i < stack; i++) {		
 		for (int j = 0; j < slice; j++) {
-			alfa = j*(2 * M_PI)/slice;
+			alfa = j * dimSide;
 			if (i == 0){
-				vertex_list.push_back(new Vertex(0,i*l,0));
+				// desenhar a base
+				vertex_list.push_back(new Vertex(0,0,0));
 
-				x = radius * sin(alfa + 2*M_PI/slice);
-				z = radius * cos(alfa + 2*M_PI/slice);
-				vertex_list.push_back(new Vertex(x,i*l,z));
+				x = radius * sin(alfa + dimSide);
+				z = radius * cos(alfa + dimSide);
+				vertex_list.push_back(new Vertex(x,0,z));
 
 				x = radius * sin(alfa);
 				z = radius * cos(alfa);
-				vertex_list.push_back(new Vertex(x,i*l,z));
+				vertex_list.push_back(new Vertex(x,0,z));
 			}
 			if (i < stack - 1) {
-				x = (radius - ((i+1)*t)) * sin(alfa + 2*M_PI/slice);
-				z = (radius - ((i+1)*t)) * cos(alfa + 2*M_PI/slice);
+				// desenhar 2 triangulos para um lado (slice)
+				x = (radius - ((i+1)*t)) * sin(alfa + dimSide);
+				z = (radius - ((i+1)*t)) * cos(alfa + dimSide);
 				vertex_list.push_back(new Vertex(x,(i+1)*l,z));
 				
 				x = (radius - ((i+1)*t)) * sin(alfa);
@@ -38,21 +41,22 @@ vector<Vertex*> createSphere(float radius, float height, int slice, int stack){
 
 				vertex_list.push_back(new Vertex(x,i*l,z));
 				
-				x = (radius - (i*t)) * sin(alfa + 2*M_PI/slice);
-				z = (radius - (i*t)) * cos(alfa + 2*M_PI/slice);
+				x = (radius - (i*t)) * sin(alfa + dimSide);
+				z = (radius - (i*t)) * cos(alfa + dimSide);
 				vertex_list.push_back(new Vertex(x,i*l,z));
 				
-				x = (radius - ((i+1)*t)) * sin(alfa + 2*M_PI/slice);
-				z = (radius - ((i+1)*t)) * cos(alfa + 2*M_PI/slice);
+				x = (radius - ((i+1)*t)) * sin(alfa + dimSide);
+				z = (radius - ((i+1)*t)) * cos(alfa + dimSide);
 				vertex_list.push_back(new Vertex(x,(i+1)*l,z));
 			}
 			else {
+				// desenhar o topo do cone
 				x = (radius - (i*t)) * sin(alfa);
 				z = (radius - (i*t)) * cos(alfa);
 				vertex_list.push_back(new Vertex(x,i*l,z));
 
-				x = (radius - (i*t)) * sin(alfa + 2*M_PI/slice);
-				z = (radius - (i*t)) * cos(alfa + 2*M_PI/slice);
+				x = (radius - (i*t)) * sin(alfa + dimSide);
+				z = (radius - (i*t)) * cos(alfa + dimSide );
 				vertex_list.push_back(new Vertex(x,i*l,z));
 
 				vertex_list.push_back(new Vertex(0,(i+1)*l,0));
